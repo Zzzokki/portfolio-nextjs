73c9ee989401cdff7afb315c31aaa7fed711ba30
Zzzokki <dispositionz30k@gmail.com>
2024-07-03 12:35:06 +0800

    hi

--- src/app/page.js
+++ src/app/page.js
@@ -4,33 +4,7 @@ import { GridContainer } from "@/components/GridContainer";
 import { Card } from "../components/Card";
 import { useState } from "react";
 import { Navbar } from "@/components/Navbar";
-
-const data = [
-  {
-    title: "Hello",
-    description: "Description",
-  },
-  {
-    title: "Title",
-    description: "Description",
-  },
-  {
-    title: "Ronaldo",
-    description: "Description",
-  },
-  {
-    title: "Lebron",
-    description: "Description",
-  },
-  {
-    title: "Messi",
-    description: "Description",
-  },
-  {
-    title: "Sengur",
-    description: "Description",
-  },
-];
+import { AllBlogs } from "@/components/AllBlogs";
 
 export default function Home() {
   const [isDark, setIsDark] = useState(false);
@@ -44,11 +18,7 @@ export default function Home() {
       <div className="w-full min-h-[100vh] bg-[#f5f6f5] dark:bg-[#000]">
         <Navbar toggleTheme={handleClick} isDark={isDark} />
 
-        <GridContainer>
-          {data.map(({ title, description }) => (
-            <Card key={title} title={title} description={description} />
-          ))}
-        </GridContainer>
+        <AllBlogs />
       </div>
     </div>
   );
---
+++ src/components/TreindingBlogs.js
@@ -0,0 +1,13 @@
+export const TrendingBlogs = () => {
+  return (
+    <div className="flex flex-col m-auto w-full max-w-[1100px]">
+      <h1 className="font-bold text-2xl">Trending</h1>
+
+      <div className="flex gap-5"></div>
+    </div>
+  );
+};
+
+const TrendingCard = ({ title, tags }) => {
+  return <></>;
+};
